b7c5bc8e981609c653b14546a7371b484dd91f17
kakashi <nk29time@gmail.com>
2021-06-05 19:07:03 +0900

    second

--- mission2.php
+++ mission2.php
@@ -1,15 +1,20 @@
 <?php
 function FizzBuzz($Fizz,$Buzz):void{
-  if(!preg_match('/^[0-9]+$/', $Fizz)  || !preg_match('/^[0-9]+$/', $Buzz)){
-    echo '整数値を入力してください。';
-    return;
-  } else {
-     for ($i = 1;$i < 100;$i++) {
-        if ($i % $Fizz == 0) echo 'Fizz '.$i.'<br>';
-        if ($i % $Buzz == 0) echo 'Buzz '.$i.'<br>';
-        if ($i % $Fizz == 0 && $i % $Buzz == 0) echo 'FizzBuzz '.$i.'<br>';
-     }
-   }
+    if (!preg_match('/^[0-9]+$/', $Fizz)  || !preg_match('/^[0-9]+$/', $Buzz)) {
+        echo '整数値を入力してください。';
+        return;
+    } else {
+        for ($i = 1;$i < 100;$i++) {
+            if ($i % $Fizz == 0 && $i % $Buzz == 0) {
+                echo 'FizzBuzz '.$i.'<br>' ;
+            } else if ($i % $Fizz == 0) {
+                echo 'Fizz '.$i.'<br>';
+            } else if ($i % $Buzz == 0) {
+                echo 'Buzz '.$i.'<br>';
+            }
+        }
+        return;
+    }
 }
 ?>
 
